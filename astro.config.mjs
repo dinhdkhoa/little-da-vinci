045d2cd550fafb462fccbda0b2aa57fc10eb286d
dinhdkhoa <kevin_mccain1718@yahoo.com>
2026-06-17 20:39:24 +0700

    feat: Keystatic integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,29 @@
-import { defineConfig, passthroughImageService } from "astro/config";
+import { defineConfig, passthroughImageService, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+import keystatic from "@keystatic/astro";
 
 export default defineConfig({
     site: 'https://dinhdkhoa.github.io',
-    base: '/little-da-vinci',
+    base: process.env.NODE_ENV === 'production' ? '/little-da-vinci' : undefined,
     prefetch: true,
+    output: 'static',
     vite: {
         plugins: [tailwindcss()],
         envDir: "../../",
     },
+    integrations: [
+        react(),
+        process.env.NODE_ENV === 'production' ? null : keystatic()
+    ].filter(Boolean),
     image: {
         service: passthroughImageService()
     },
+    fonts: [{
+        name: "Phudu",
+        provider: fontProviders.google(),
+        cssVariable: "--font-phudu",
+    }],
     i18n: {
         locales: ["en", "vi"],
         defaultLocale: "vi",
